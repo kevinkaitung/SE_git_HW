e006a7e8040727b6c315b867b70ddab53f25cec7
kevinkaitung <dadado0570@gmail.com>
2021-12-03 12:07:16 +0800

    init commit

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int total = 0;
+    int i;
+    for (i = 1; i <= 10; i++)
+    {
+        total += i;
+    }
+    return 0;
+}
